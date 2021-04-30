bc62acb7fb183b4099c79d2ba2d515f24bfae926
“ritikbobade” <ritikbobade@gmail.com>
2021-04-30 19:30:18 +0530

    intergrating climate in main flask

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from keras import backend as K
 
 from flask import *
 from cropmodel import prediction
+from cropclimate import climate 
 
 app = Flask(__name__, template_folder='template')
 K.clear_session()
@@ -24,6 +25,19 @@ def result():
         output = prediction(result)
         print(output)
         return render_template("result.html", result=result, output=output)
+
+@app.route('/climate')
+def climate_index():
+    return render_template('climate_index.html')
+
+
+@app.route('/climate', methods=['POST','GET'])
+def climate_result():
+    if request.method == 'POST':
+        climate_result = request.form
+        output=climate(climate_result)
+        print(output)
+        return render_template("climate_result.html",result=climate_result,output=output)        
 
 if __name__ == '__main__':
     app.run(debug=True)
